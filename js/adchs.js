27e44b9248c9234263abc538068028a9e0dabf16
Sun Junwen <sunjw8888@gmail.com>
2014-02-01 16:44:17 +0800

    Generate menu from js.

--- js/adchs.js
+++ js/adchs.js
@@ -3,10 +3,210 @@
  * License: GPL 2.0
  * Author: Sun Junwen
  */
+
+var curUrl = window.location.href;
+
+var secondLevPath = ["building-blocks/", "get-started/", "patterns/", "style/"];
+var secondLevPathCount = secondLevPath.length;
+
 // Menu Setup
-jqFolding.setup({
-	initState : "hide"
-});
+var menuContent = [
+	[{
+			text : "开始",
+			url : "index.html"
+		}, {
+			text : "创新的视觉效果",
+			url : "get-started/create-vistion.html"
+		}, {
+			text : "设计原则",
+			url : "get-started/principles.html"
+		}, {
+			text : "UI 概览",
+			url : "get-started/ui-overview.html"
+		}
+	], [{
+			text : "风格",
+			url : "style/index.html"
+		}, {
+			text : "设备和显示",
+			url : "style/devices-displays.html"
+		}, {
+			text : "主题",
+			url : "style/themes.html"
+		}, {
+			text : "触摸反馈",
+			url : "style/touch-feedback.html"
+		}, {
+			text : "度量单位和网格",
+			url : "style/metrics-grids.html"
+		}, {
+			text : "字体",
+			url : "style/typography.html"
+		}, {
+			text : "颜色",
+			url : "style/color.html"
+		}, {
+			text : "图标",
+			url : "style/iconography.html"
+		}, {
+			text : "写作风格",
+			url : "style/writing.html"
+		}
+	], [{
+			text : "模式",
+			url : "patterns/index.html"
+		}, {
+			text : "Android 新特性",
+			url : "patterns/new.html"
+		}, {
+			text : "手势",
+			url : "patterns/gestures.html"
+		}, {
+			text : "应用结构",
+			url : "patterns/app-structure.html"
+		}, {
+			text : "导航",
+			url : "patterns/navigation.html"
+		}, {
+			text : "操作栏",
+			url : "patterns/actionbar.html"
+		}, {
+			text : "多视图布局",
+			url : "patterns/multi-pane-layouts.html"
+		}, {
+			text : "滑动视图",
+			url : "patterns/swipe-views.html"
+		}, {
+			text : "选择",
+			url : "patterns/selection.html"
+		}, {
+			text : "确认和提示",
+			url : "patterns/confirming-acknowledging.html"
+		}, {
+			text : "通知",
+			url : "patterns/notifications.html"
+		}, {
+			text : "小部件",
+			url : "patterns/widgets.html"
+		}, {
+			text : "设置",
+			url : "patterns/settings.html"
+		}, {
+			text : "帮助",
+			url : "patterns/help.html"
+		}, {
+			text : "兼容性",
+			url : "patterns/compatibility.html"
+		}, {
+			text : "可用性",
+			url : "patterns/accessibility.html"
+		}, {
+			text : "纯粹的 Android",
+			url : "patterns/pure-android.html"
+		}
+	], [{
+			text : "控件",
+			url : "building-blocks/index.html"
+		}, {
+			text : "标签选项卡",
+			url : "building-blocks/tabs.html"
+		}, {
+			text : "列表",
+			url : "building-blocks/lists.html"
+		}, {
+			text : "网格列表",
+			url : "building-blocks/grid-lists.html"
+		}, {
+			text : "滚动容器",
+			url : "building-blocks/scrolling.html"
+		}, {
+			text : "下拉菜单 (Spinners)",
+			url : "building-blocks/spinners.html"
+		}, {
+			text : "按钮",
+			url : "building-blocks/buttons.html"
+		}, {
+			text : "文本框",
+			url : "building-blocks/text-fields.html"
+		}, {
+			text : "滑块",
+			url : "building-blocks/seek-bars.html"
+		}, {
+			text : "进度条和活动",
+			url : "building-blocks/progress.html"
+		}, {
+			text : "开关",
+			url : "building-blocks/switches.html"
+		}, {
+			text : "对话框",
+			url : "building-blocks/dialogs.html"
+		}, {
+			text : "选择器",
+			url : "building-blocks/pickers.html"
+		}
+	]
+];
+
+function fillMenu() {
+	var i = 0, j = 0;
+	var menuCont = $("#navWrapper");
+	var curPage = curUrl.split("/");
+	curPage = curPage[curPage.length - 1]; // *.html
+	for(i = 0; i < secondLevPathCount; ++i) {
+		if (curUrl.search(secondLevPath[i]) != -1) {
+			curPage = secondLevPath[i] + curPage;
+		}
+	}
+
+	var firstLevCount = menuContent.length;
+	
+	for (i = 0; i < firstLevCount; ++i) {
+		var foldingList = $("<div/>");
+		foldingList.addClass("divListwHeader");
+		foldingList.addClass("foldingList");
+
+		var listCont = $("<ul/>");
+		listCont.addClass("foldingContainer");
+
+		var secondLevMenu = menuContent[i];
+		var secondLevCount = secondLevMenu.length;
+		for (j = 0; j < secondLevCount; ++j) {
+			var menuItem = secondLevMenu[j];
+			var menuUrl = menuItem.url;
+
+			if (curPage.search("/") != -1) {
+				menuUrl = "../" + menuUrl;
+			}
+
+			var link = $("<a/>");
+			link.attr("href", menuUrl);
+			link.html(menuItem.text);
+
+			if (curPage == menuItem.url) {
+				// current page
+				foldingList.addClass("initShow");
+				link.addClass("highlight");
+			}
+
+			if (j == 0) {
+				// menu header
+				var listHeader = $("<div/>");
+				listHeader.addClass("trigger");
+				listHeader.append(link);
+				foldingList.append(listHeader);
+			} else {
+				var linkCont = $("<li/>");
+				linkCont.append(link);
+				listCont.append(linkCont);
+			}
+		}
+
+		foldingList.append(listCont);
+		menuCont.append(foldingList);
+	}
+
+	menuCont.append($("<br/>").addClass("clear"));
+}
 
 function menuFix() {
 	var scrollLeftPos = (window.pageXOffset ||
@@ -31,6 +231,24 @@ function menuFix() {
 		navWrapper.css("left", pageWrapperLeft - scrollLeftPos + "px");
 		navWrapper.css("height", windowHeight - 20 + "px");
 	}
+}
+
+function initMenu() {
+	fillMenu();
+
+	jqFolding.setup({
+		initState : "hide"
+	});
+	jqFolding.init();
+
+	// Menu position fix
+	window.onscroll = function () {
+		menuFix();
+	}
+	window.onresize = function () {
+		menuFix();
+	}
+	menuFix();
 }
 
 // Acronym tooltip
@@ -89,19 +307,18 @@ function replaceAll(str, find, replace) {
 
 function fillFooter() {
 	var footerCont = $("#footer");
+	var i = 0;
 
-	var curUrl = window.location.href;
 	var secondLevel = 0;
-	if (curUrl.search("building-blocks/") != -1 ||
-		curUrl.search("get-started/") != -1 ||
-		curUrl.search("patterns/") != -1 ||
-		curUrl.search("style/") != -1) {
-		secondLevel = 1;
+	for(i = 0; i < secondLevPathCount; ++i) {
+		if (curUrl.search(secondLevPath[i]) != -1) {
+			secondLevel = 1;
+		}
 	}
 
 	var lines = footerLines.length;
-	var i = 0;
-	for (; i < lines; ++i) {
+	
+	for (i = 0; i < lines; ++i) {
 		var line = footerLines[i];
 		if (secondLevel) {
 			line = replaceAll(line, "imgs/", "../imgs/");
@@ -119,16 +336,7 @@ function fillFooter() {
 
 // Init
 $(function () {
-	jqFolding.init();
-
-	// Menu position fix
-	window.onscroll = function () {
-		menuFix();
-	}
-	window.onresize = function () {
-		menuFix();
-	}
-	menuFix();
+	initMenu();
 
 	// Auto play video
 	$('video.clickPlay').click(function () {
